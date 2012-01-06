88790ad37db8fb9849b42f9f8f7bb36db89df171
Juan A. Romero <rukano@gmail.com>
2012-01-06 14:54:49 +0100

    booting server example

---
+++ example3.lua
@@ -0,0 +1,16 @@
+Server = require('sclua.Server')
+Synth = require('sclua.Synth')
+
+s = Server:new()
+s:boot()
+
+go(2, function ()
+      print("loading synths")
+      s:sendMsg('/d_loadDir', "/Users/rukano/Library/Application\ Support/SuperCollider/synthdefs")
+      wait(1)
+      a = Synth:new("default")
+      wait(1)
+      a:set{"gate", 0}
+      wait(1)
+      a:free()
+end)
