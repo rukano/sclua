6adc62961ef646f4ee8a81a29bc085aacccfd81d
Juan A. Romero <rukano@gmail.com>
2012-01-06 13:54:13 +0100

    server options 90% done

--- sclua/Server.lua
+++ sclua/Server.lua
@@ -1,62 +1,106 @@
 local osc = require("osc")
 --local funcs = require("sclua.funcs")
 
+local function tobinary (bool) if bool return 1 else return 0 end
 local Server = {}
 Server.__index = Server
 
 --------------------
 -- Server Options
+
 Server.options = {}
 Server.options.path = "/Applications/LuaAV.12.12.11/"
+Server.options.ip = '127.0.0.1'
 Server.options.port = 57110
+Server.options.numAudioBusChannels = 128
+Server.options.numControlBusChannels = 4096
+Server.options.numInputBusChannels = 2
+Server.options.numOutputBusChannels = 2
+Server.options.numBuffers = 1026
+Server.options.maxNodes = 1024
+Server.options.maxSynthDefs = 1024
+Server.options.protocol = 'u' -- u for utp, t for tcp
+Server.options.blockSize = 64
+Server.options.hardwareBufferSize = nil
+Server.options.memSize = 32768
+Server.options.numRGens = 64
+Server.options.numWireBufs = 64
+Server.options.sampleRate = nil
+Server.options.loadDefs = false
+Server.options.verbosity = 0
+Server.options.zeroConf = false
+Server.options.device = nil
+Server.options.pluginsPath = nil
 
---[[
-   -u <udp-port-number>    a port number 0-65535
-   -t <tcp-port-number>    a port number 0-65535
-   -c <number-of-control-bus-channels> (default 4096)
-   -a <number-of-audio-bus-channels>   (default 128)
-   -i <number-of-input-bus-channels>   (default 8)
-   -o <number-of-output-bus-channels>  (default 8)
-   -z <block-size>                     (default 64)
-   -Z <hardware-buffer-size>           (default 0)
-   -S <hardware-sample-rate>           (default 0)
-   -b <number-of-sample-buffers>       (default 1024)
-   -n <max-number-of-nodes>            (default 1024)
-   -d <max-number-of-synth-defs>       (default 1024)
-   -m <real-time-memory-size>          (default 8192)
-   -w <number-of-wire-buffers>         (default 64)
-   -r <number-of-random-seeds>         (default 64)
-   -D <load synthdefs? 1 or 0>         (default 1)
-   -R <publish to Rendezvous? 1 or 0>  (default 1)
-   -l <max-logins>                     (default 64)
-          maximum number of named return addresses stored
-          also maximum number of tcp connections accepted
-   -p <session-password>
-          When using TCP, the session password must be the first command sent.
-          The default is no password.
-          UDP ports never require passwords, so for security use TCP.
-   -N <cmd-filename> <input-filename> <output-filename> <sample-rate> <header-format> <sample-format>
-   -I <input-streams-enabled>
-   -O <output-streams-enabled>
-   -M <server-mach-port-name> <reply-mach-port-name>
-   -H <hardware-device-name>
-   -v <verbosity>
-          0 is normal behaviour
-          -1 suppresses informational messages
-          -2 suppresses informational and many error messages
-   -U <ugen-plugins-path>    a colon-separated list of paths
-          if -U is specified, the standard paths are NOT searched for plugins.
-   -P <restricted-path>    
-          if specified, prevents file-accessing OSC commands from
-          accessing files outside <restricted-path>.
---]]
+-- possible future options to use:
+-- Server.options.inputStreamsEnabled = nil
+-- Server.options.outputStreamsEnabled = nil
+-- Server.options.blockAllocClass = nil
+-- Server.options.restrictedPath = nil
+-- Server.options.initialNodeID = 1000
+-- Server.options.remoteControlVolume = false
+-- Server.options.memoryLocking = false
 
------------------------------
+
+-- scsynth unused options :
+
+-- -l <max-logins>                     (default 64)
+-- maximum number of named return addresses stored
+-- also maximum number of tcp connections accepted
+-- -p <session-password>
+-- When using TCP, the session password must be the first command sent.
+-- The default is no password.
+-- UDP ports never require passwords, so for security use TCP.
+-- -N <cmd-filename> <input-filename> <output-filename> <sample-rate> <header-format> <sample-format> -- nonrealtime
+-- -I <input-streams-enabled>
+-- -O <output-streams-enabled>
+-- -M <server-mach-port-name> <reply-mach-port-name>
+-- -P <restricted-path>    
+-- if specified, prevents file-accessing OSC commands from
+-- accessing files outside <restricted-path>.
+
+function Server:boot()
+   local o = self.options
+   local cmd =
+      "cd " .. o.path
+      .. " scsynth &&"
+      .. " -" .. o.protocol .. " " .. o.port
+      .. " -c " .. o.numControlBusChannels
+      .. " -a " .. o.numAudioBusChannels
+      .. " -i " .. o.numInputBusChannels
+      .. " -o " .. o.numOutputBusChannels
+      .. " -z " .. o.blockSize
+      .. " -Z " .. (o.hadwareBufferSize or 0)
+      .. " -S " .. (o.sampleRate or 0)
+      .. " -b " .. o.numBuffers
+      .. " -n " .. o.maxNodes
+      .. " -d " .. o.maxSynthDefs
+      .. " -m " .. o.memorySize
+      .. " -w " .. o.numWireBufs
+      .. " -r " .. o.numRGens
+      .. " -D " .. tobinary(o.loadDefs)
+      .. " -R " .. tobinary(o.zeroConf)
+      .. " -v " .. o.verbosity
+
+   if o.device then
+      cmd = cmd .. " -H " .. '\"'..  o.device .. '\"'  -- enquote?
+   end
+
+   if o.pluginsPath then
+      cmd .. " -U " .. o.pluginsPath
+   end
+
+   cmd = cmd .." &" -- run in background
+   
+   os.execute(cmd) 
+end
+
+
 function Server:new(IP, port)
 	local srv = {}
 	setmetatable(srv, Server)
-	IP = IP or '127.0.0.1'
-	port = port or 57110
+	local IP = IP or self.ip
+	local port = port or self.port
 	srv.IP = IP
 	srv.port = port
 	oscout = osc.Send(srv.IP, srv.port)
@@ -71,15 +115,6 @@ function Server:dumpOSC(mode)
 --	2 - print the contents in hexadecimal.
 --	3 - print both the parsed and hexadecimal representations of the contents.	
 	oscout:send('/dumpOSC', mode)
-end
-
-function Server:boot()
-	os.execute(
-	   "cd " .. self.options.path
-	      .. " scsynth &&"
-	      .. " -u " .. self.options.port
-	      ..  " -b 1026 -R 0 &"
-		  ) 
 end
 
 function Server:freeAll()
